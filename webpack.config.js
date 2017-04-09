a6c78e18e3022f08baa20507aa38e9060c411f5c
Evan Boyle <evboyle@microsoft.com>
2017-04-09 16:08:22 -0700

    adding redux/react as peer dependencies, moving to externals for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,11 @@ module.exports = {
         // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
     },
-
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM",
+        "redux": "Redux"
+    },
     module: {
         loaders: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
